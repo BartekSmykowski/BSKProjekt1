b85dae04c58bfbdfeb10c788d6483ac89c30e3ed
janusz96 <bsmykowski@wp.pl>
2018-03-12 16:45:56 +0100

    fxml controlls bind

--- src/main/java/sample/controller/EncodeSceneController.java
+++ src/main/java/sample/controller/EncodeSceneController.java
@@ -1,10 +1,18 @@
 package sample.controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import sample.scenesManage.ScenesManager;
 import sample.scenesManage.ScenesNames;
 
 public class EncodeSceneController {
+    public TextField newFileNameTextField;
+    public Label chosenFileLabel;
+    public Label fileSizeLabel;
+    public Label extensionLabel;
+    public Label encodingType;
+
     public void mainMenu(MouseEvent mouseEvent) {
         ScenesManager.setScene(ScenesNames.MENU);
     }
--- src/main/java/sample/controller/RegisterSceneController.java
+++ src/main/java/sample/controller/RegisterSceneController.java
@@ -1,13 +1,21 @@
 package sample.controller;
 
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import sample.scenesManage.ScenesManager;
 import sample.scenesManage.ScenesNames;
 
 public class RegisterSceneController {
 
+    public TextField loginTextField;
+    public PasswordField passwordField;
+
     public void mainMenu(MouseEvent mouseEvent) {
         ScenesManager.setScene(ScenesNames.MENU);
     }
 
+    public void register(MouseEvent mouseEvent) {
+
+    }
 }
