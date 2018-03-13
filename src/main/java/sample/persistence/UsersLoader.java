38bbcd4ed39baa201a83503f3f0d5d4d26db41d4
janusz96 <bsmykowski@wp.pl>
2018-03-13 15:05:57 +0100

    encoding data

--- src/main/java/sample/persistence/UsersLoader.java
+++ src/main/java/sample/persistence/UsersLoader.java
@@ -1,17 +1,16 @@
 package sample.persistence;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import sample.Settings;
+import sample.exception.CannotReadUsersException;
+import sample.model.User;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.Collection;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import sample.Settings;
-import sample.exception.CannotReadUsersException;
-import sample.model.User;
 
 public class UsersLoader {
 	private final ObjectMapper objectMapper = new ObjectMapper();
@@ -28,7 +27,7 @@ public class UsersLoader {
 	}
 
 	private Map<String, String> loadUsers(String path) {
-		URL users = getClass().getResource("/" + Settings.USERS_WITH_PRIVATE_KEY_PATH);
+		URL users = getClass().getResource("/" + path);
 		try {
 			return objectMapper.readValue(users, new TypeReference<Map<String, String>>() {
 			});
